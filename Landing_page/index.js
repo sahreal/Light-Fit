2f5aa79941af554eefd1a331c22fa261cb2c3eee
cferdinand <chrisferdinand07@gmail.com>
2020-04-22 11:07:52 -0400

    initial commit with changes to the index.html and index.js files for the landing page and bot. Added some new dependencies as well

--- Landing_page/index.js
+++ Landing_page/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const axios = require("axios");
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-const port = 4000;
+app.get("/app-slack-oauth", (req, res) => {
+  const body = `code=${req.query.code}&client_id=${process.env.CLIENTID}&client_secret=${process.env.CLIENTSECRET}&redirect_uri=https://slackbot-coolbot.herokuapp.com/app-slack-oauth`;
+  const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+
+  axios
+    .post("https://slack.com/api/oauth.v2.access", body, { headers })
+    .then(resp => {
+      res.send(resp.data);
+    })
+    .catch(err => {
+      console.log(`ERROR: ${JSON.stringify(err.response.data)}`);
+    });
+});
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
   console.log(`Landing page app listening at http://localhost:${port}`)
--- SlackBot/index.js
+++ SlackBot/index.js
@@ -1,80 +1,117 @@
-const { WebClient } = require("@slack/web-api");
-const SlackToken = require("../SlackBot/token/SlackToken");
-const axios = require("axios");
-
-// Read a token from the environment variables
-const token = SlackToken.token;
-// Initialize
-const web = new WebClient(token);
-
-// Given some known conversation ID (representing a public channel, private channel, DM or group DM)
-const conversationId = "general";
-
-(async () => {
-  // Post a message to the channel, and await the result.
-  // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-
-  let name = await axios
-    .get(`https://pokeapi.co/api/v2/pokemon/charmander`)
-    .then(res => {
-      //console.log(result.data);
-      let pokemons = {
-        pic: res.data.sprites.front_default,
-        name: res.data.name
-      };
-      return pokemons;
-    })
-    .catch(err => console.log(err));
-
-  const result = await web.chat.postMessage({
-    text: "WOW",
-    unfurl_media: true,
-    //attachments: [{ title: name.name, image_url: name.pic }],
-    channel: conversationId
-  });
-
-  // The result contains an identifier for the message, `ts`.
-  console.log(
-    `Successfully send message ${result.ts} in conversation ${conversationId}`
-  );
-})();
-
-//-------------------------------------------------------------------------------------------------------------------
-//** Ignore below code. Was using to experiment with Slack's new bot, the current working code is Slack's Classic bot
-//-------------------------------------------------------------------------------------------------------------------
-
-// const SlackBot = require("slackbots");
+/*====================================
+CLASSIC SLACK BOT
+======================================*/
+// const { WebClient } = require("@slack/web-api");
+// const SlackToken = require("../SlackBot/token/SlackToken");
 // const axios = require("axios");
 
-// const bot = new SlackBot({
-//   token: INSERT_TOKEN
-//   name: "testtest"
-// });
+// // Read a token from the environment variables
+// const token = SlackToken.token;
+// // Initialize
+// const web = new WebClient(token);
 
-// bot.on("start", () => {
-//   const params = {
-//     icon_emoji: ":cat:"
-//   };
+// // Given some known conversation ID (representing a public channel, private channel, DM or group DM)
+// const conversationId = "general";
 
-//   bot.postMessageToChannel(
-//     "general",
-//     "WOOOOOOO SLACK APPPPPPPPPPPPPP",
-//     params
+// (async () => {
+//   // Post a message to the channel, and await the result.
+//   // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+
+//   let name = await axios
+//     .get(`https://pokeapi.co/api/v2/pokemon/charmander`)
+//     .then(res => {
+//       //console.log(result.data);
+//       let pokemons = {
+//         pic: res.data.sprites.front_default,
+//         name: res.data.name
+//       };
+//       return pokemons;
+//     })
+//     .catch(err => console.log(err));
+
+//   const result = await web.chat.postMessage({
+//     text: "WOW",
+//     unfurl_media: true,
+//     //attachments: [{ title: name.name, image_url: name.pic }],
+//     channel: conversationId
+//   });
+
+//   // The result contains an identifier for the message, `ts`.
+//   console.log(
+//     `Successfully send message ${result.ts} in conversation ${conversationId}`
 //   );
+// })();
 
-//   //bot.postMessageToUser("user_name", "Hello world!", params);
-// });
+//-------------------------------------------------------------------------------------------------------------------
+// CURRENT SLACK BOT
+//-------------------------------------------------------------------------------------------------------------------
 
-// //message handler
-// bot.on("message", data => {
-//   if (data.type !== "message") {
-//     return;
-//   } else {
-//     let name = data.text.split(" ");
+const { WebClient } = require("@slack/web-api");
+const CronJob = require("cron").CronJob;
 
-//     let result = getPokemon(name[1]);
-//     return result;
-//   }
-// });
+// Gets the slack bot token from environment variables
+const token = process.env.TOKEN;
 
-// bot.on("error", err => console.log(err));
+// Initialize web client connection with the token
+const bot = new WebClient(token);
+
+// CronJob schedules posts to be sent
+// TODO: How to send posts to channels outside the initial installed workspace
+(async () => {
+  var morningJob = new CronJob(
+    "00 00 9 * * 1-5",
+    async function() {
+      await bot.chat.postMessage({
+        channel: "coolbot-test-sandbox",
+        text:
+          "Hey I am coolBot. I post a daily 9am message. Get some coffee :coffee"
+      });
+    },
+    null,
+    true,
+    "America/New_York"
+  );
+  var middayJob = new CronJob(
+    "00 00 12 * * 1-5",
+    async function() {
+      await bot.chat.postMessage({
+        channel: "coolbot-test-sandbox",
+        text:
+          "Hey I am coolBot. I post a daily 12pm message. Get some lunch! That work will still be there after. TRUST ME."
+      });
+    },
+    null,
+    true,
+    "America/New_York"
+  );
+  var afternoonJob = new CronJob(
+    "00 00 15 * * 1-5",
+    async function() {
+      await bot.chat.postMessage({
+        channel: "coolbot-test-sandbox",
+        text:
+          "Hey I am coolBot. I post a daily 3pm message. It's almost clock out time! Get Excited!!"
+      });
+    },
+    null,
+    true,
+    "America/New_York"
+  );
+  var eveningJob = new CronJob(
+    "00 00 18 * * 1-5",
+    async function() {
+      await bot.chat.postMessage({
+        channel: "coolbot-test-sandbox",
+        text:
+          "Hey I am coolBot. I post a daily 6pmm message. Get some dinner or drinks, it's after 5 here!"
+      });
+    },
+    null,
+    true,
+    "America/New_York"
+  );
+  morningJob.start();
+  middayJob.start();
+  afternoonJob.start();
+  eveningJob.start();
+})();
