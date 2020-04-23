50f988bb2d3963680eb1dbbee815ce769c70bd02
Sahar <saharadri1@gmail.com>
2020-04-22 21:45:40 -0400

    merged

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -2,17 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const { Morning, MidDay, Afternoon, Evening } = require("./models/index");
-=======
 const axios = require("axios");
 >>>>>>> 84ba76c2e7eaffb379f243fed9803d7f1a5ba21d
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/dist/")));
 
-<<<<<<< HEAD
 const db = require("./config/key.js").mongoURI;
 
 mongoose
@@ -21,7 +18,6 @@ mongoose
   .catch(err => console.log(err));
 
 const port = 3000;
-=======
 app.get("/app-slack-oauth", (req, res) => {
   const body = `code=${req.query.code}&client_id=${process.env.CLIENTID}&client_secret=${process.env.CLIENTSECRET}&redirect_uri=https://light-fit.herokuapp.com/app-slack-oauth`;
   const headers = { "Content-Type": "application/x-www-form-urlencoded" };
@@ -37,7 +33,6 @@ app.get("/app-slack-oauth", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
->>>>>>> 84ba76c2e7eaffb379f243fed9803d7f1a5ba21d
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
