5c713d29482e1db520975ffb8014f12317a4ec46
cferdinand <chrisferdinand07@gmail.com>
2020-04-24 02:05:11 -0400

    updated app to listen for connections

--- SlackBot/index.js
+++ SlackBot/index.js
@@ -12,5 +12,11 @@ app.use("/", routes);
 // Gets the slack bot token from environment variables
 const token = process.env.TOKEN;
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
 // Schedules for the default installed workspace
 messageScheduler(token, "coolbot-test-sandbox", "America/New_York");
