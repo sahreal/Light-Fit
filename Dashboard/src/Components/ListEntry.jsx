89da2ee6b3eb6a70404ca893a165838b0aa0e585
cferdinand <chrisferdinand07@gmail.com>
2020-04-30 18:18:51 -0400

    fixed commit history due to lost reference info to List Entry

--- Dashboard/src/Components/ListEntry.jsx
+++ Dashboard/src/Components/ListEntry.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-// import "@babel/polyfill";
+import React, { useState, useRef } from "react";
 
-const ListEntry = ({ prompt, time, deleteHandler }) => {
-  const deleteH = (event) => {
-    console.log(event.target);
+const ListEntry = ({ prompt, time, deleteEntries }) => {
+  const valueRef = React.useRef(time);
+
+  const deleteH = (e) => {
+    let prompt = event.target.value;
+    let time = valueRef.current;
+
+    let obj = {
+      Prompt: prompt,
+      Time: time,
+    };
+
+    deleteEntries(obj);
   };
 
   return (
